6894d6f25e7763f0fc8dfa255fc07feee0985104
Tilek04 <tilek.dzenishev@gmail.com>
2021-08-02 17:10:22 +0600

    Finish base framework functional

--- webpack.config.js
+++ webpack.config.js
@@ -12,8 +12,10 @@ const jsLoaders = () => {
   const loaders = [
     {loader: 'babel-loader',
       options: {
-        presets: ['@babel/preset-env']
-      }}
+        presets: ['@babel/preset-env'],
+        plugins: ['@babel/plugin-proposal-class-properties']
+      }
+    }
   ]
   if (isDev) {
     loaders.push('eslint-loader')
